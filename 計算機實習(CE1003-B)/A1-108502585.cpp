53513beeab1027859d2154f30133428178bf2593
Roy Lin <44574155+chshroy@users.noreply.github.com>
2019-09-27 17:18:14 +0800

    Create A1-108502585.cpp

---
+++ 計算機實習(CE1003-B)/A1-108502585.cpp
@@ -0,0 +1,14 @@
+/*
+* Assignment 1
+* Name: 林昱求
+* Student Number: 108502585
+* Course 2019-CE1003-B
+*/
+#include <iostream>
+using namespace std;
+
+signed main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << c << b << a << endl;
+}
