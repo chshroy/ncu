a38e57a25d00aece444dacb4e813b4b4abd2f14f
Roy Lin <44574155+chshroy@users.noreply.github.com>
2019-10-01 14:36:35 +0800

    Create 函數圖形練習3.py

---
+++ 微積分/函數圖形練習3.py
@@ -0,0 +1,21 @@
+import pylab
+import numpy
+
+pi = pylab.pi
+sin = pylab.sin
+
+
+def f(x):
+    return sin(x) / abs(2 * x)
+    
+a, b, n = -10 * pi, 10 * pi, 10000
+xs = pylab.linspace(a, b, n)
+
+pylab.plot(xs, f(xs))  # draw f(x)
+title = "f(x) = sin(x)/|2x|"
+pylab.title(title, fontsize = 8) # add title
+
+
+pylab.grid()
+
+pylab.show()
