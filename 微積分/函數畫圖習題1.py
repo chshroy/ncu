a5c1b95afc08ebac9bb2048ff959a8c69ca4fbfc
Roy Lin <44574155+chshroy@users.noreply.github.com>
2019-10-07 01:48:21 +0800

    Update 函數畫圖習題1.py

--- 微積分/函數畫圖習題1.py
+++ 微積分/函數畫圖習題1.py
@@ -11,13 +11,15 @@ pi = pylab.pi
 a, b, n = -2 * pi, 2 * pi, 10000
 xs = pylab.linspace(a, b, n)
 
-pylab.plot(xs, f(xs))  # draw f(x)
-pylab.plot(xs, g(xs))  # draw g(x)
+pylab.plot(xs, f(xs), color = 'blue')  # draw f(x)
+pylab.plot(xs, g(xs), color = 'green')  # draw g(x)
 title = "f(x)=max( abs(x sin(x)), abs(x cos(x)) ), g(x)=min( abs(x sin(x)), abs(x cos(x)) )"
 pylab.title(title, fontsize = 8) # add title
 
 pylab.xlim((-8, 8))    # x-axis
 pylab.ylim((0, 7))     # y-axis
+pylab.xlabel("x")
+pylab.ylabel("y")
 pylab.grid()
 
 pylab.show()
