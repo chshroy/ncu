9bd37ab223b49310ce7bd677f3d2b6d47226e9c7
Roy Lin <44574155+chshroy@users.noreply.github.com>
2019-11-11 23:25:52 +0800

    Create 微分計算習題2.py

---
+++ 微積分/微分計算習題2.py
@@ -0,0 +1,31 @@
+import pylab
+import numpy
+
+pi = pylab.pi
+sin = pylab.sin
+cos = pylab.cos
+
+
+def f(x):
+    return pylab.sqrt(2*(sin(x)+cos(2*x)+3)/(x+1))
+    
+def df(x, h):
+    return (f(x + h) - f(x)) / h
+    
+a, b, n = 0, 20, 10000
+h = (b - a) / (n - 1)
+xs = pylab.linspace(a, b, n)
+
+pylab.plot(xs, f(xs))  # draw f(x)
+pylab.plot(xs, df(xs, h), color = 'green')
+title = "f(x) = sqrt(2*(sin(x)+cos(2*x)+3)/(x+1)) and computed derivative"
+pylab.title(title, fontsize = 8) # add title
+
+pylab.xlim((a, b))    # x-axis
+pylab.ylim((-1, 2))     # y-axis
+pylab.xlabel("x")
+pylab.ylabel("y")
+pylab.grid()
+
+pylab.show()
+
